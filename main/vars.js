0a35e5b66ac5b1e9edb32ead78946ddd7e1b5854
King Lucifer <119492378+LuciferDevOp@users.noreply.github.com>
2024-08-30 05:51:01 +0000

    -

--- main/vars.js
+++ main/vars.js
@@ -1,20 +1,35 @@
 
-import dotenv from 'dotenv';
-dotenv.config();
+// import dotenv from 'dotenv';
+// dotenv.config();
 
-const botToken = process.env.botToken;
-const masterId = process.env.masterId;
-const LogGroupId = process.env.LogGroupId;
+var data;
+
+async function getvars() {
+    const URL = `https://mybot-md2-default-rtdb.firebaseio.com/mobotVars/.json`;
+    const response = await fetch(URL);
+    data = await response.json();
+}
+getvars();
+const botToken = data.botToken;
+const masterId = data.masterId;
+const LogGroupId = data.LogGroupId;
+const dbLink = data.dbLink;
+const userDbLink = data.userDbLink;
+const token = data.token;
+
+// const botToken = process.env.botToken;
+// const masterId = process.env.masterId;
+// const LogGroupId = process.env.LogGroupId;
 const channels = ["MDistinct"];
 
 const startLogId = 12;
 const deployLogId = 19;
 const errorLogId = 21;
 
-const dbLink = process.env.dbLink;
-const userDbLink = process.env.userDbLink;
+// const dbLink = process.env.dbLink;
+// const userDbLink = process.env.userDbLink;
 
-const token = process.env.token;
+// const token = process.env.token;
 const github = {
     token,
     owner: "MdMobid"
